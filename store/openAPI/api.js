168223d47eba149a650e6dab34662c6049628720
Crowdbotics <engineering@crowdbotics.com>
2023-03-29 23:39:42 +0000

    Update store.

--- store/openAPI/api.js
+++ store/openAPI/api.js
@@ -1,8 +1,12 @@
 import axios from "axios"
-import {} from "react-native-dotenv"
+import { OPENAPI_TOKEN } from "react-native-dotenv"
 const openAPI = axios.create({
   baseURL: "https://api.openai.com/v1",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${OPENAPI_TOKEN}`
+  }
 })
 function openapi_get_models_read(payload) {
   return openAPI.get(`/models`)
