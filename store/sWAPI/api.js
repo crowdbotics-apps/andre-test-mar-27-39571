1dc6e1721b249d988b12e868053f67e9c150f847
Crowdbotics <engineering@crowdbotics.com>
2023-03-29 02:06:42 +0000

    Update store.

--- store/sWAPI/api.js
+++ store/sWAPI/api.js
@@ -3,7 +3,7 @@ const sWAPI = axios.create({
   baseURL: "https://swapi.dev/api/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-function swapi_get_people_read(payload) {
-  return sWAPI.get(`/people`)
+function swapi_get_people_id_read(payload) {
+  return sWAPI.get(`/people/${payload.id}`)
 }
-export const apiService = { swapi_get_people_read }
+export const apiService = { swapi_get_people_id_read }
--- store/sWAPI/swapi_response_get_Peopledetails.slice.js
+++ store/sWAPI/swapi_response_get_Peopledetails.slice.js
@@ -1,9 +1,9 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const swapi_get_people_read = createAsyncThunk(
-  "swapi_response_get_Peopledetails/swapi_get_people_read",
+export const swapi_get_people_id_read = createAsyncThunk(
+  "swapi_response_get_Peopledetails/swapi_get_people_id_read",
   async payload => {
-    const response = await apiService.swapi_get_people_read(payload)
+    const response = await apiService.swapi_get_people_id_read(payload)
     return response.data
   }
 )
@@ -13,12 +13,12 @@ const swapi_response_get_PeopledetailsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
-    [swapi_get_people_read.pending]: (state, action) => {
+    [swapi_get_people_id_read.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [swapi_get_people_read.fulfilled]: (state, action) => {
+    [swapi_get_people_id_read.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities = [
           ...state.entities.filter(record => record.id !== action.payload.id),
@@ -27,7 +27,7 @@ const swapi_response_get_PeopledetailsSlice = createSlice({
         state.api.loading = "idle"
       }
     },
-    [swapi_get_people_read.rejected]: (state, action) => {
+    [swapi_get_people_id_read.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
@@ -36,6 +36,6 @@ const swapi_response_get_PeopledetailsSlice = createSlice({
   }
 })
 export default {
-  swapi_get_people_read,
+  swapi_get_people_id_read,
   slice: swapi_response_get_PeopledetailsSlice
 }
