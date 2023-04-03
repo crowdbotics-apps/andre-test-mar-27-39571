781df7501c9a45be9e3b4a3ae6d1fd9be44e6971
Andre Machado <andre.machado@crowdbotics.com>
2023-04-03 17:03:29 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "andre-test-mar-27-39571",
   "url": "https://andre-test-mar-27-39571.botics.co",
   "api": "https://andre-test-mar-27-39571.botics.co/api/v1",
-  "initialRoute": "Untitled1"
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
