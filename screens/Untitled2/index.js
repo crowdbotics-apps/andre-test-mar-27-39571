7cb80a98249e763f5bf51dce1cd05248365fd7d2
Andre Machado <andre.machado@crowdbotics.com>
2023-04-25 20:18:11 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled2/index.js
@@ -0,0 +1,43 @@
+import { openapi_get_models_read } from "../../store/openAPI/openapi_response_get_ListModels.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled2 = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(openapi_get_models_read());
+  }, []);
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      flex: 1,
+      flexDirection: 'column',
+      height: '100%',
+      padding: 10,
+      backgroundColor: '#f0f0f1'
+    }}>
+        
+          <View style={styles.column1}></View>
+          <View style={styles.column2}></View>
+          <View style={styles.column3}></View>
+        
+        </ScrollView>
+      </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  column1: {
+    flex: 1
+  },
+  column2: {
+    flex: 1
+  },
+  column3: {
+    flex: 1
+  }
+});
+export default Untitled2;
