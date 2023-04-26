167f8c2e32b200815e509e109ad4d52fc66b9a07
Andre Machado <andre.machado@crowdbotics.com>
2023-04-25 20:25:49 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
+import { Text } from "react-native";
 import { openapi_get_models_read } from "../../store/openAPI/openapi_response_get_ListModels.slice.js";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import React from "react";
 import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled2 = () => {
+  const {
+    entities: openapi_response_get_ListModels
+  } = useSelector(state => state.openapi_response_get_ListModels);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(openapi_get_models_read());
@@ -18,7 +22,7 @@ const Untitled2 = () => {
       backgroundColor: '#f0f0f1'
     }}>
         
-          <View style={styles.column1}></View>
+          <View style={styles.column1}><Text style={styles.jZXlZIlU}>model ID: {openapi_response_get_ListModels[0]?.data[0]?.id}</Text><Text style={styles.mNhuZhiG}>model created: {openapi_response_get_ListModels[0]?.data[0]?.created}</Text><Text style={styles.sXFnsKTM}>model owned_by: {openapi_response_get_ListModels[0]?.data[0]?.owned_by}</Text></View>
           <View style={styles.column2}></View>
           <View style={styles.column3}></View>
         
@@ -38,6 +42,27 @@ const styles = StyleSheet.create({
   },
   column3: {
     flex: 1
+  },
+  jZXlZIlU: {
+    width: 335,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  mNhuZhiG: {
+    width: 334,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  sXFnsKTM: {
+    width: 335,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled2;
